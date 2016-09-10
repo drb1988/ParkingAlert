289d76eb161fb322e4ad56d7dc7b079195b9ea4e
Claudia Oprea <Claudia Oprea>
2016-09-10 23:02:16 +0300

    users

--- node/routes/index.js
+++ node/routes/index.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/users', function(req, res, next) {
+  res.render('users', { title: 'Express' });
+});
+
+router.get('/users/:userId', function(req, res, next) {
+  res.render('user', { title: 'Express' });
+});
+
 module.exports = router;
