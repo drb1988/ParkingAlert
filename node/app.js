9e0174c7a850f61ca4731a5f722e9e25ec295e0e
Claudia Oprea <Claudia Oprea>
2016-09-10 22:21:28 +0300

    bootstrap & views

--- node/app.js
+++ node/app.js
@@ -56,8 +56,8 @@ app.use(function(err, req, res, next) {
   });
 });
 
-app.listen( 3000 , function () {
+app.listen(3000, function() {
   console.log("App listening on port " + 3000 );
-  });
+});
 
 module.exports = app;
