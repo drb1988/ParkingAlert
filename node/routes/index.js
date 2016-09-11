bf56604c27f9b5f5229d92da896afd0d1ac98a2e
Drb <bogdan.drb@bigcityapps.ro>
2016-09-12 00:00:51 +0300

    Added live documentation module and samples

--- node/routes/index.js
+++ node/routes/index.js
@@ -3,14 +3,27 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+	/**
+    * Base route,
+    * @name /
+    */
   res.render('index', { title: 'Express' });
 });
 
 router.get('/users', function(req, res, next) {
+	/**
+    * Route to get all users,
+    * @name /users
+    */
   res.render('users', { title: 'Express' });
 });
 
 router.get('/users/:userId', function(req, res, next) {
+    /**
+    * Route to get users by ID,
+    * @name /users/:userId
+    * @param :userId
+    */
   res.render('user', { title: 'Express' });
 });
 
