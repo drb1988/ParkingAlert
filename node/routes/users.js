006e5417d596bb2ba166cc725d6fe006adf5d94a
drb1988 <bogdan.drb@bigcityapps.ro>
2016-09-20 15:36:20 +0300

    added routes to add user added route to add car to a user added route to add security options

--- node/routes/users.js
+++ node/routes/users.js
@@ -1,5 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var dbConfig = require('../db');
+var db = require('mongoskin').db(dbConfig.url);
+var assert = require('assert');
+var MongoClient = require('mongodb').MongoClient;
+var ObjectId = require('mongodb').ObjectId; 
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -9,5 +14,107 @@ router.get('/', function(req, res, next) {
 router.get('/chart', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+router.post('/user', function(req, res, next) {
+    /**
+    * Route to get users by ID,
+    * @name /users/:userId
+    * @param {String} :userId
+    */
+  var userID ="";
+  var insertDocument = function(db, callback) {
+   db.collection('parking').insertOne( {
+      "first_name": req.body.first_name,
+      "last_name": req.body.last_name,
+      "nickname": req.body.nickname,
+      "email": req.body.email,
+      "cars": [],
+      "security": [],
+      "driver_license": req.body.driver_license,
+      "photo": req.body.photo,
+      "platform": req.body.platform,
+      "user_city": req.body.user_city
+   }, function(err, result) {
+    assert.equal(err, null);
+    userID = result.insertedId;
+    console.log("Inserted a user in the users collection. "+result.insertedId);
+    callback();
+  });
+};
+MongoClient.connect(dbConfig.url, function(err, db) {
+  assert.equal(null, err);
+  insertDocument(db, function() {
+      db.close();
+      res.status(200).send(userID)
+  });
+});
+});
+
+router.post('/addCar/:userID', function(req, res, next) {
+    /**
+    * Route to get users by ID,
+    * @name /users/:userId
+    * @param {String} :userId
+    */
+    var insertCar = function(db, callback) {   
+ 	var o_id = new ObjectId(req.params.userID);
+    db.collection('parking').update({"_id": o_id}, 
+             {$push: { 
+                         "cars":{ 	
+                        			"plates": req.body.plates,
+      								"given_name": req.body.given_name,
+      								"make": req.body.make,
+      								"model": req.body.model,
+      								"year": req.body.year 
+      							} 
+                      }
+             },function(err, result) {
+				    assert.equal(err, null);
+				    console.log("Inserted a car for the user "+req.params.userID);
+				    callback();
+			});            
+	}
+	MongoClient.connect(dbConfig.url, function(err, db) {
+		  assert.equal(null, err);
+		  insertCar(db, function() {
+		      db.close();
+		      res.status(200).send(req.params.userID)
+		  });
+		});
+})
+
+router.post('/addSecurity/:userID', function(req, res, next) {
+    /**
+    * Route to get users by ID,
+    * @name /users/:userId
+    * @param {String} :userId
+    */
+    var insertSecurity = function(db, callback) {   
+ 	var o_id = new ObjectId(req.params.userID);
+    db.collection('parking').update({"_id": o_id}, 
+             {$push: { 
+                         "security":{ 	
+                        			"device_token": req.body.device_token,
+                        			"password": req.body.password,
+                        			"created_at": new Date(),
+                        			"updated_at": new Date(),
+                        			"last_auth": new Date(), 
+                        			"reg_ip": req.body.reg_ip 
+      								} 
+                      }
+             },function(err, result) {
+				    assert.equal(err, null);
+				    console.log("Inserted security options for the user "+req.params.userID);
+				    callback();
+			});            
+	}
+	MongoClient.connect(dbConfig.url, function(err, db) {
+		  assert.equal(null, err);
+		  insertSecurity(db, function() {
+		      db.close();
+		      res.status(200).send(req.params.userID)
+		  });
+		});
+})
 
 module.exports = router;
