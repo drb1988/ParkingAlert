e4f0bf79d2b964fac7d9dc46685c4aeac54e9f93
Claudia Oprea <claudiaoprea16@gmail.com>
2016-09-20 17:04:43 +0300

    chart page

--- node/public/js/main.js
+++ node/public/js/main.js
@@ -1,8 +1,6 @@
 $(document).ready(function() {
   togleIsDaily();
   togleIsMultiple();
-  initDatePicker();
-  initTinyMCE();
 });
 
 
@@ -20,32 +18,3 @@ function togleIsMultiple() {
   });
 }
 
-function initDatePicker() {
-  $("#datepicker").datepicker({
-    dateFormat: "yy-mm-dd"
-  });
-  $("#datepickerStart").datepicker({
-    dateFormat: "yy-mm-dd"
-  });
-  $("#datepickerEnd").datepicker({
-    dateFormat: "yy-mm-dd"
-  });
-}
-
-function initTinyMCE() {
-  tinymce.init({
-    selector: '.tiniymce',
-    height: 500,
-    plugins: [
-      "advlist autolink lists link image charmap print preview anchor",
-      "searchreplace visualblocks code fullscreen",
-      "insertdatetime media table contextmenu paste imagetools"
-    ],
-    toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image",
-    imagetools_cors_hosts: ['www.tinymce.com', 'codepen.io'],
-    content_css: [
-      '//fast.fonts.net/cssapi/e6dc9b99-64fe-4292-ad98-6974f93cd2a2.css',
-      '//www.tinymce.com/css/codepen.min.css'
-    ]
-  });
-}
