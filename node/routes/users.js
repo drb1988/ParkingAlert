549ed3d538a19948be59c921b1d54c620bfe1596
drb1988 <bogdan.drb@bigcityapps.ro>
2016-10-14 12:27:38 +0300

    Updated the route for car editing Made QR code generating a lot more random

--- node/routes/users.js
+++ node/routes/users.js
@@ -5,6 +5,22 @@ var db = require('mongoskin').db(dbConfig.url);
 var assert = require('assert');
 var MongoClient = require('mongodb').MongoClient;
 var ObjectId = require('mongodb').ObjectId; 
+var jwt = require('json-web-token');
+var Chance = require('chance');
+var chance = new Chance();
+
+var generateCode = function (user)
+ {	
+ 	var secret = "Friendly2016"
+ 	var seed = chance.integer({min: 100000, max: 999999}).toString();
+ 	var payload = {
+        "user_id"   : user,
+        "seed"     : seed,
+        "usage": "QRCode"
+    };
+    var result = jwt.encode( secret, payload);
+    return result.value;
+ }
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -240,17 +256,11 @@ router.post('/editCar/:userID&:plates', function(req, res, next) {
  	var o_id = new ObjectId(req.params.userID);
     db.collection('parking').update({"_id": o_id, "cars.plates":req.params.plates}, 
              {$set: { 
-                         "cars":[{ 	
-                        			"plates": req.body.plates,
-      								"given_name": req.body.given_name,
-      								"make": req.body.make,
-      								"model": req.body.model,
-      								"year": req.body.year,
-      								"enable_notifications": req.body.enable_notifications,
-      								"qr_code": req.body.qr_code,
-      								"is_owner": req.body.is_owner,
-      								"enable_others": req.body.enable_others  
-      							}]  
+                        "cars.$.plates": req.body.plates,
+      					"cars.$.given_name": req.body.given_name,
+      					"cars.$.make": req.body.make,
+      					"cars.$.model": req.body.model,
+      					"cars.$.year": req.body.year,
                       }
              },function(err, result) {
 				    assert.equal(err, null);
@@ -405,7 +415,7 @@ router.get('/getUsersForCode/:token', function(req, res, next) {
 
 router.get('/generateCarCode/:userID', function(req, res, next) {
 	res.status(200).send({
-		"carCode":  "66"+req.params.userID+"33" 
+		"carCode":  generateCode(req.params.userID) 
 	})
 });
 
