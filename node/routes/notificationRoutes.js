74155b48bb97328c26251d0009c50afaaab64967
drb1988 <bogdan.drb@bigcityapps.ro>
2016-11-02 09:56:54 +0200

    Forcing locations to be parsed as float

--- node/routes/notificationRoutes.js
+++ node/routes/notificationRoutes.js
@@ -180,8 +180,8 @@ router.post('/notification', function(req, res, next) {
       "location": {
           "type": "Point",
           "coordinates": [
-            req.body.latitude,
-            req.body.longitude
+            parseFloat(req.body.latitude),
+            parseFloat(req.body.longitude)
           ]
       },
       "reverse_geocode": req.body.reverse_geocode,
@@ -197,8 +197,8 @@ router.post('/notification', function(req, res, next) {
         {"sender": {
           "type": "Point",
           "coordinates": [
-            req.body.latitude,
-            req.body.longitude
+            parseFloat(req.body.latitude),
+            parseFloat(req.body.longitude)
           ]}}
       ],
       "receiver_token": notificationReceiverToken,
@@ -326,8 +326,8 @@ router.post('/receiverAnswered/:notificationID', function(req, res, next) {
                       "receiver": {
                           "type": "Point",
                           "coordinates": [
-                            req.body.latitude,
-                            req.body.longitude
+                            parseFloat(req.body.latitude),
+                            parseFloat(req.body.longitude)
                           ]}
                     }   
               }
